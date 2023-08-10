6d61cd66b20d00474424069dc2abeb25fd4cea22
David-Jacks <djackfaith0.5@gmail.com>
2023-08-10 09:09:48 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,14 +3,11 @@
 /**
  * main - Entry point
  *
- * Return: will be 1 (Success)
+ * Return: will be 1 (Standard erro)
  */
 int main(void)
 {
-	int i;
-	char my_ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	for (i = 0; my_ch[i] != '\0'; i++)
-		putchar(my_ch[i]);
-	putchar('\n');
+	fprintf(stderr,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
