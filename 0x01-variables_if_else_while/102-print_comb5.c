b0bc060c67af0cea667616e1cce4b29e5f0eb2a2
David-Jacks <djackfaith0.5@gmail.com>
2023-08-11 17:21:40 -0700

    finally completed

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,10 +8,32 @@
 
 int main(void)
 {
+	int k;
 	int i;
+	int j;
+	int m;
 
 	for (i = 48; i < 58; i++)
-		putchar((char)i);
+	{
+		for (j = 48; j < 58; j++)
+		{
+			for (k = i; k < 58; k++)
+			{
+				for (m = j + 1; m < 58; m++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(m);
+					if (i ==57 && j == 56 && k == 57 && m == 57)
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
 	putchar('\n');
 	return (0);
 }
