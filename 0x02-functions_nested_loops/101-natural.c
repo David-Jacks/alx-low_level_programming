179410fb8af275a1adaa3385a0255d3631cf0f03
David-Jacks <djackfaith0.5@gmail.com>
2023-08-15 19:27:35 -0700

    completed task

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,13 +2,13 @@
 #include "main.h"
 /**
  * main - function to print absolute values
- * @n: a variable to contain the number to be checked
+ *
  * Return: returns 0 for end of program
  */
 
 int main(void)
 {
-	int sum;
+	int sum = 0;
 	int i;
 
 	for (i = 3; i < 1024; i++)
