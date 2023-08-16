50f7189e24e0e8e0f5e1fc31896583c67f31280e
David-Jacks <djackfaith0.5@gmail.com>
2023-08-15 22:06:45 -0700

    done

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,17 +8,15 @@
 
 int print_last_digit(int n)
 {
-	int ans;
+	int ans = n % 10;
 
 	if (n & (0x80000000))
 	{
-		ans = (n % 10) * -1;
-		putchar(ans + '0');
-		return (ans);
+		_putchar(-ans + '0');
+		return (-ans);
 	}
 	else
 	{
-		ans = n % 10;
 		putchar(ans + '0');
 		return (ans);
 	}
