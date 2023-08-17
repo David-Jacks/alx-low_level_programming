b009f17696fa185c8fb265f97bd8b2439e163e37
David-Jacks <djackfaith0.5@gmail.com>
2023-08-17 15:10:12 -0700

    done

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *more_numbers - print numerals
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j < 15; j++)
+			_putchar(j + '0');
+		_putchar('\n');
+	}
+}
