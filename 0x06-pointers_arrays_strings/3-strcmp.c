a5a6d9002127e6ddb52f0d710a836d807f8d6318
David-Jacks <djackfaith0.5@gmail.com>
2023-08-24 09:26:28 -0700

    done

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,23 +2,43 @@
 #include "main.h"
 
 /**
- *_strncpy - coppies strings
- *@src: string to be added
- *@dest: string to be added to
- *@n: number of src characters to be added
- *Return: returns a pointer to dest
+ *_strcmp - compares strings
+ *@s1: string 1
+ *@s2: string 2
+ *Return: returns an integer value
  */
 
-char *_strncpy(char *dest, char *src, int n)
+int _strcmp(char *s1, char *s2)
 {
 	int j = 0;
+	int i = 0;
+	int cnt = 0;
 
-	while (src[j] != '\0' && j < n)
+	while (s1[j] != '\0')
 	{
-		dest[j] = src[j];
 		j++;
 	}
-	dest[j] = '\0';
 
-	return (dest);
+	while (s2[i] != '\0')
+	{
+		i++;
+	}
+	if (i == j)
+	{
+		i = 0;
+		while (s1[i] != '\0')
+		{
+			if (s1[i] == s2[i])
+				cnt++;
+			i++;
+		}
+
+		if (j == cnt)
+			return (0);
+	}
+	else if (j < i)
+		return (-1);
+	else
+		return (1);
+	return (1);
 }
